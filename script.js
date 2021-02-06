ec42a5879c624e1bdfb34ee4ef64ec242e1dfe43
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-06 23:53:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,78 @@
-//document.getElementsByTagName('button').bgcolor="white";
 var a = randomColor();
 var b = randomColor();
 var c = randomColor();
 var colorArray = [a,b,c];
-var color = 'rgb(1,2,255)'
+var choosen_id;
+var choosen_color;
+var score = 0;
 
 document.getElementById("button1").style.backgroundColor= a;
 document.getElementById("button2").style.backgroundColor= b;
 document.getElementById("button3").style.backgroundColor= c;
 
-var choosen = colorArray[Math.floor(Math.random() * 2)];
+var guess = colorArray[Math.floor(Math.random() * 2)];
 
-
-document.getElementById('colorguess').innerHTML = choosen;
+document.getElementById('colorguess').innerHTML = guess;
 
 function randomColor(){
-    var x = Math.floor(Math.random() * 255);
-    var y = Math.floor(Math.random() * 255);
-    var z = Math.floor(Math.random() * 255);
-    return 'rgb('+ x +','+ y +','+ z+')'
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return 'rgb('+ r +','+ g +','+ b +')'
 }
+
+function getIde(clicked_id) {
+    choosen_id = clicked_id;
+    choosen();
+}
+
+function choosen(){
+    switch (choosen_id) {
+        case 'button1':
+            choosen_color = a;
+            //alert(choosen_color);
+            break;
+        case 'button2':
+            choosen_color = b;
+            //alert(choosen_color);
+            break;
+        case 'button3':
+            choosen_color = c;
+            //alert(choosen_color);
+            break;
+        default:
+            break;
+    }
+    if(choosen_color == guess){
+        // alert('win!!');
+        //score=;
+        document.getElementById("hna").innerHTML = ++score;
+        a = randomColor();
+        b = randomColor();
+        c = randomColor();
+        colorArray = [a,b,c];
+        choosen_id = null;
+        choosen_color = null;
+        document.getElementById("button1").style.backgroundColor= a;
+        document.getElementById("button2").style.backgroundColor= b;
+        document.getElementById("button3").style.backgroundColor= c;
+
+        guess = colorArray[Math.floor(Math.random() * 2)];
+
+        document.getElementById('colorguess').innerHTML = guess;
+        //score = 0;
+        //location.reload();
+        //return false;
+    }
+     
+    else{
+        // alert('win!!');
+        //score--;
+        document.getElementById("hna").innerHTML = --score;
+        document.getElementById(choosen_id).style.backgroundColor= 'black';
+
+        //location.reload();
+        //return false;
+    }
+}
+
